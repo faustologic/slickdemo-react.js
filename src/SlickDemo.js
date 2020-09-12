b19350923a2ba5115d5623c651c5aaa46c65a3e3
faustologic <69010388+faustologic@users.noreply.github.com>
2020-09-12 16:31:13 -0500

    Updated

---
+++ src/SlickDemo.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'  
+import "slick-carousel/slick/slick.css";  
+import "slick-carousel/slick/slick-theme.css";  
+import Slider from "react-slick";  
+import './SlickDemo.css';  
+
+export class SlickDemo extends Component {  
+    render() {  
+        var settings = {  
+          dots: false, 
+          infinite: true,  
+          speed: 700,  
+          centerMode: true,  
+          slidesToShow: 1,  
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2300,
+          pauseOnHover: false,
+          fade: true   
+          };  
+          return (  
+            <div className="slides">    
+            <Slider {...settings} >    
+              <div className="wdt">  
+                <img  src= {'assets/W3.jpg'} className="img"/>  
+              </div>  
+              <div className="wdt">  
+                <img src= {'assets/W4.jpg'} className="img"/>  
+              </div>  
+              <div className="wdt">  
+                <img src= {'assets/W5.jpg'} className="img"/>  
+              </div >  
+              <div className="wdt">  
+                <img src= {'assets/W6.jpg'} className="img"/>  
+              </div>    
+            </Slider>  
+            </div>  
+          );  
+        }  
+      }  
+  
+export default SlickDemo
